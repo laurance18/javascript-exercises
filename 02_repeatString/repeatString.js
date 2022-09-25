866d956bf8321d4f8625b53c3b0d40b2cc849082
Deniz Işık <denizisik2004@gmail.com>
2022-09-25 17:53:58 +0300

    Solution added for exercise-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let return_string = "";
+    if(num<0) return "ERROR";
+    
+    for (let i = 0; i++, i<num+1;) {
+        return_string += str;
+    }
+    return return_string;
 };
 
 // Do not edit below this line
