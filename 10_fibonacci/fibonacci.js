d1ee9ccf8656b61a5d0abefacd59a3d0e8dd7999
Deniz Işık <denizisik2004@gmail.com>
2022-09-27 23:14:59 +0300

    Solution added for exercise-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if(pos<0) return 'OOPS';
+    pos = Number(pos) 
+    
+    arr = [1,1];
+    initLength = arr.length;
+    if(arr[pos] === undefined){
+        for(let i = 0; i<pos-initLength; i++){
+            firstNum = arr[arr.length-2]
+            lastNum = arr[arr.length-1]
+            arr.push(firstNum+lastNum);
+        }
+        return arr[pos-1]
+    }
+    return arr[pos-1]
 };
 
 // Do not edit below this line
