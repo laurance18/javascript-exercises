d968be9ca6a186ba32ad056c3c9e372db2fd6991
Deniz Işık <denizisik2004@gmail.com>
2022-09-27 20:30:08 +0300

    Solution added for exercise-09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    str = str.replace(/\s{2,}/g," ");
+    str = str.replace(/ /g,'');
+    str = str.toLowerCase()
+    str.trim();
+    return(str == str.split("").reverse().join(""));
 };
 
 // Do not edit below this line
