927cc6fe5282414864b2f76d5bab8439262a317a
Deniz Işık <denizisik2004@gmail.com>
2022-09-27 20:14:36 +0300

    Solution added for exercise-08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach(function (num) {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.forEach(function (num){
+    total *= num;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for(let i=1; i<=num; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
