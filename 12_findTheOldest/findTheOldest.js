840fdb112e17a113ea81b52a7941de1a7394bcf1
Deniz Işık <denizisik2004@gmail.com>
2022-09-27 23:46:58 +0300

    Solution added for exercise-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let ages = [];
+    arr.forEach(function (person) {
+        String(typeof(person['yearOfDeath'])) == 'undefined' ? person['yearOfDeath'] = 2022 : console.log('')
+        ages.push(person['yearOfDeath']-person['yearOfBirth'])
+    });
+    return arr[ages.indexOf(Math.max(...ages))];
 };
 
 // Do not edit below this line
